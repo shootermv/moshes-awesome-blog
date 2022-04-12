2159cfd088a1618f7c8993e96d685944a76d307b
Moshe Vilner <moshe.vilner@nielsen.com>
2022-04-12 21:53:44 +0300

    first commit

---
+++ app/routes/posts/index.tsx
@@ -0,0 +1,39 @@
+import { Link, useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import { getPosts } from "~/models/post.server";
+type Post = {
+    slug: string;
+    title: string;
+};
+
+type LoaderData = {
+    posts: Awaited<ReturnType<typeof getPosts>>;
+};
+export const loader = async () => {
+    return json<LoaderData>({
+        posts: await getPosts(),
+    });
+};
+export default function Posts() {
+    const { posts } = useLoaderData() as LoaderData;
+
+    return (
+        <main>
+            <Link to="admin" className="text-red-600 underline">
+                Admin
+            </Link>
+            <ul>
+                {posts.map((post: Post) => (
+                    <li key={post.slug}>
+                        <Link
+                            to={post.slug}
+                            className="text-blue-600 underline"
+                        >
+                            {post.title}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </main>
+    );
+}
