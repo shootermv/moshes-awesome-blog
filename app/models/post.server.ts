882e10988336aa9a73931a54d0412bd11f7ded14
Moshe Vilner <moshe.vilner@nielsen.com>
2022-04-12 22:03:19 +0300

    remove extra prisma import

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,6 +1,5 @@
 import { prisma } from "~/db.server";
 
-import { prisma } from "~/db.server";
 export type { Post } from "@prisma/client";
 export async function getPosts() {
   return prisma.post.findMany();
